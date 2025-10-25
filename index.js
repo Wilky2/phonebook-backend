42e95c6440643c84e2f5356895ce2e18d35f25ae
Wilky2 <wilkyclervil@yahoo.com>
2025-10-25 10:04:06 -0400

    Exo 3.21 done

---
+++ index.js
@@ -0,0 +1,115 @@
+require('dotenv').config()
+const express = require('express')
+const Person = require('./models/person')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+
+morgan.token('body', (req) => {
+  if (['POST', 'PUT', 'PATCH'].includes(req.method)) {
+    return JSON.stringify(req.body);
+  }
+  return '';
+});
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+app.use(express.static('dist'))
+
+app.get('/api/persons', (request, response, next) => {
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
+  .catch(error => next(error))
+})
+
+app.post('/api/persons', (request, response, next) => {
+    const body = request.body
+
+    const person = new Person({
+        name: body.name,
+        number: body.number
+    })
+
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  Person.findById(request.params.id)
+    .then(person => {
+      if (!person) {
+        return response.status(404).end()
+      }
+
+      person.content = body.content
+      person.number= body.number
+
+      return person.save().then((updatedPerson) => {
+        response.json(updatedPerson)
+      })
+    })
+    .catch(error => next(error))
+})
+
+app.get('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id
+    Person.findById(id).then(person => {
+      if(person){
+        response.json(person)
+        return
+      }
+      response.status(404).end()
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+    const id = request.params.id
+    Person.findByIdAndDelete(id).then(person => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+app.get('/info', (request, response, next) => {
+  Person.countDocuments().then(length=>{
+    const html = `
+      <p>Phonebook has info for ${length} ${length > 1 ? 'people' : 'person'}</p>
+      <p>${new Date().toString()}</p>
+    `
+    response.send(html)
+  })
+  .catch(error => next(error))
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  
+  if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
